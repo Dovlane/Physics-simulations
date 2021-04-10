0fa2fef22565577ca08235eda79b55467bb8d45c
Dovlane <57462728+Dovlane@users.noreply.github.com>
2021-04-10 20:25:43 +0200

    Bouncing_balls
    I've made class Ball, which contains attributes and methods needed to simulate ball bouncing properly.

---
+++ BouncingBall/BouncingBall/Ball.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingBall
+{
+    class Ball
+    {
+        double xBall, yBall;
+        double r;
+        public double Velocity_X {get; set;}
+        public double Velocity_Y { get; set; }
+
+        static double g = 9.81;
+        static double widthOfForm, heightOfForm;
+        static double delta_t;
+        public static void bordersOfForm(double _widthOfForm, double _heightOfForm)
+        {
+            widthOfForm = _widthOfForm;
+            heightOfForm = _heightOfForm;
+        }
+        public static void timeTickChanged(int timerInterval)
+        {
+            delta_t = 1.0 / timerInterval * 10;
+        }
+
+        public Ball(double xBall, double yBall)
+        {
+            this.xBall = xBall;
+            this.yBall = yBall;
+            this.Velocity_X = 0.0;
+            this.Velocity_Y = 0.0;
+            this.r = 20.0;
+        }
+
+        private bool inVerticalBoundaries(double y, double r)
+        {
+            return y - r >= 0 && y + r <= heightOfForm;
+        }
+        private bool inHorisontalBoundaries(double x, double r)
+        {
+            return x - r >= 0 && x + r <= widthOfForm;
+        }
+        private void placeInBallBackInForm(double smallerBorder, double largerBorder, double currentPosition)
+        {
+            if (Math.Abs(smallerBorder - currentPosition) < Math.Abs(largerBorder - currentPosition))
+                currentPosition = smallerBorder + r;
+            else
+                currentPosition = largerBorder - r;
+        }
+
+        public void changeVelocity(string Velocity_X_input, string Velocity_Y_input)
+        {
+            double result;
+            if (double.TryParse(Velocity_X_input, out result))
+                Velocity_X = result;
+            if (double.TryParse(Velocity_Y_input, out result))
+                Velocity_Y = result;
+        }
+
+        public void Paint(PaintEventArgs e)
+        {
+            SolidBrush sb = new SolidBrush(Color.Blue);
+            e.Graphics.FillEllipse(sb, (float)(xBall - r), (float)(yBall - r), (float)(2 * r), (float)(2 * r));
+
+            if (!inHorisontalBoundaries(xBall, r))
+            {
+                Velocity_X *= -1;
+                placeInBallBackInForm(0, widthOfForm, xBall);
+            }
+            if (!inVerticalBoundaries(yBall, r))
+            {
+                Velocity_Y *= -1;
+                placeInBallBackInForm(0, heightOfForm, yBall);
+            }
+            Velocity_Y += g * delta_t;
+            yBall += Velocity_Y * delta_t;
+            xBall += Velocity_X * delta_t;
+        }
+    }
+}
---
+++ BouncingBall/BouncingBall/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingBall
+{
+    // class Ball \ Ball()
+    // Add limits for values of xBall and yBall
+
+    // Make a list of Balls, and make chosenBall variable
+    
+    public partial class Form1 : Form
+    {
+        Ball ball = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ball.bordersOfForm(ClientRectangle.Width * 1.0, ClientRectangle.Height * 1.0);
+            Ball.timeTickChanged(timer1.Interval);
+
+            ball = new Ball(ClientRectangle.Width * 1.0 / 2, ClientRectangle.Height * 3.0 / 4);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Stop")
+            {
+                label1.Visible = true;
+                textBox1.Visible = true;
+                label2.Visible = true;
+                textBox2.Visible = true;
+
+                button1.Text = "Continue";
+            }
+            else if (button1.Text == "Continue")
+            {
+                ball.changeVelocity(textBox1.Text, textBox2.Text);
+
+                label1.Visible = false;
+                textBox1.Visible = false;
+                label2.Visible = false;
+                textBox2.Visible = false;
+
+                button1.Text = "Stop";
+            }
+        }
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Ball.bordersOfForm(ClientRectangle.Width * 1.0,
+                ClientRectangle.Height * 1.0);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (ball != null)
+                ball.Paint(e);
+        }
+
+    }
+}
